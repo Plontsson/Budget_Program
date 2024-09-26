7cf354dc9a7459621a65d4203aa4488bfef13e19
Duntis <jonathan.pettersson2002@gmail.com>
2024-09-26 09:29:54 +0200

    Update 26/9
    Added functions to tidy up the code a bir.
    
    Also added so that when you choose "Val 1". Instead of directly after going to the main menu, you now get the option to add another category directly. To make it less tedious.

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 budget_posts = {} #dictionary with all budget posts and its values
 
+def calculated_costs():
+    costs = sum(budget_posts.values()) #function to sum up all the costs
+    return costs
+
+def calculated_excess(salary): #function to calculated excess salary
+    excess = salary - calculated_costs()
+    return excess
+
+
 print("Välkommen till ditt budgetprogram") # Start of program
 while True:
     salary = input("Vad är din lön?")
     try:
         salary = float(salary)
+        break
     except ValueError:
         print("Du måste skriva en siffra!")
 
@@ -16,13 +26,18 @@ while True: #Main loop
     svar = input("Välj vad du vill göra: ")
 
     if svar == "Val 1": #Add a budget post
-        key = input("Vilken kategori skulle du vilja lägga till? ") #ask for the budgetpost/key
-        if key in budget_posts:#checks if the budget post already  exists
-            print ("Den kategorin finns redan!")
-        else:
-            value = float(input("Vad kostar denna kategori? "))
-            budget_posts[key] = value
-
+        while True:
+            key = input("Vilken kategori skulle du vilja lägga till? ") #ask for the budgetpost/key
+            if key in budget_posts:#checks if the budget post already  exists
+                print ("Den kategorin finns redan!")
+            else:
+                value = float(input("Vad kostar denna kategori? "))
+                budget_posts[key] = value
+                question = input("Vill du lägga till en kategori till? Ja/Nej ")
+                if question == "Yes":
+                    pass
+                else:
+                    break
     elif svar == "Val 2": # Change budgetpost
         key = input("Vilken kategori skulle du vilja ändra? ")
         if key in budget_posts: #checks if budget post already exists
@@ -32,11 +47,9 @@ while True: #Main loop
             print("Den budgetposten finns inte!") 
         
     elif svar == "Val 3": # Val 3 loop
-        costs = sum(budget_posts.values())
-        excess = salary - costs
         print(budget_posts)
-        print("Summan av alla dina kostnader är: ", costs)
-        print("Du har såhär mycket pengar kvar: ", excess)
+        print("Summan av alla dina kostnader är: ", calculated_costs())
+        print("Du har såhär mycket pengar kvar: ", calculated_excess(salary))
         break
     elif svar == "Val 4": #Avslutar programmet
         ("Avslutar...")
