409d771696e29e4ee68733bbb2e1b5474e7af6f1
Duntis <jonathan.pettersson2002@gmail.com>
2024-09-26 10:22:49 +0200

    Update
    changed some lingo, comments, lowercase everything

--- app.py
+++ app.py
@@ -19,40 +19,40 @@ while True:
         print("Du måste skriva en siffra!")
 
 while True: #Main loop
-    print("Val 1: Lägg till kategori.")
-    print("Val 2: Ändra Kategori.")
+    print("Val 1: Lägg till budgetpost.")
+    print("Val 2: Ändra budgetpost.")
     print("Val 3: Skriv ut budget.")
     print("Val 4: Avsluta program.")
-    svar = input("Välj vad du vill göra: ")
+    svar = input("Välj vad du vill göra: ").lower()
 
-    if svar == "Val 1": #Add a budget post
+    if svar == "val 1": #Add a budget post
         while True:
-            key = input("Vilken kategori skulle du vilja lägga till? ") #ask for the budgetpost/key
+            key = input("Vilken budgetpost skulle du vilja lägga till? ").lower() #ask for the budgetpost/key
             if key in budget_posts:#checks if the budget post already  exists
-                print ("Den kategorin finns redan!")
+                print ("Den budgetposten finns redan!")
             else:
-                value = float(input("Vad kostar denna kategori? "))
+                value = float(input("Vad kostar denna budgetpost? "))
                 budget_posts[key] = value
-                question = input("Vill du lägga till en kategori till? Ja/Nej ").lower()
+                question = input("Vill du lägga till en budgetpost till? Ja/Nej ").lower() #checks if user wants to add another budgetpost before remaining to main loop
                 if question == "ja":
                     continue
                 else:
                     break
-    elif svar == "Val 2": # Change budgetpost
-        key = input("Vilken kategori skulle du vilja ändra? ")
+    elif svar == "val 2": # Change budgetpost
+        key = input("Vilken kategori skulle du vilja ändra? ").lower()
         if key in budget_posts: #checks if budget post already exists
             value = float(input("Hur mycket kostar kategorin? "))
             budget_posts[key] = value
         else:
             print("Den budgetposten finns inte!") 
         
-    elif svar == "Val 3": # Val 3 loop
+    elif svar == "val 3": # Val 3 loop
         print(budget_posts)
         print("Summan av alla dina kostnader är: ", calculated_costs())
         print("Du har såhär mycket pengar kvar: ", calculated_excess(salary))
         break
-    elif svar == "Val 4": #Avslutar programmet
-        ("Avslutar...")
+    elif svar == "val 4": #Avslutar programmet
+        print("Avslutar...")
         break
     else:
         print("Du måste välja ett av dom fyra valen!")
