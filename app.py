0409a4867b6c0c010527352c96ff29c84ddab7f6
Plontsson <jonathan.pettersson2002@gmail.com>
2024-09-26 19:24:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
+import os
+
 budget_posts = {} #dictionary with all budget posts and its values
+
+def load_file(): #function for loading previous budget
+    if os.path.exists("budget.txt"): #checks if there is a budget file on system already
+        with open("budget.txt", "r") as file: #opens file
+            for line in file:
+                if "Summan av dina kostnäder är: " in line or "Du har såhär mycket över: " in line:
+                    continue #skips the sum costs and excess salary
+                key, value = line.split(": ")#seperate budget post and value
+                budget_posts[key] = float(value.replace("kr", "").strip())#convert value to float, remove "kr" and strip spaces
+    print("Din tidigare budget har laddats in!")
+
 
 def calculated_costs():
     costs = sum(budget_posts.values()) #function to sum up all the costs
@@ -9,6 +22,9 @@ def calculated_excess(salary): #function to calculated excess salary
     return excess
 
 print("Välkommen till ditt budgetprogram") # Start of program
+
+load_file()
+
 while True:
     salary = input("Vad är din lön? ")
     try:
@@ -64,7 +80,8 @@ while True: #Main loop
                 print("Den budgetposten finns inte!") 
         
     elif svar == "val 3": #Printing budgetpost
-        print(f"{key}: {value}", "kr")
+        for key, value in budget_posts.items():
+            print(f"{key}: {value}", "kr")
         print("Summan av alla dina kostnader är: ", calculated_costs(), "kr")
         print("Du har såhär mycket pengar kvar: ", calculated_excess(salary), "kr")
         question = input("Vill du spara detta till en fil? ja/nej ").lower() #Question user if to create txt file of budget
@@ -72,9 +89,9 @@ while True: #Main loop
         if question == "ja": 
             file = open("budget.txt", "w")
             for key, value in budget_posts.items():
-                file.write(f"{key}: {value}\n")
-                file.write(f"Summan av dina kostnader är: {calculated_costs()}\n") #write the calculated costs to the budget file
-                file.write(f"Du har såhär mycket pengar över: {calculated_excess(salary)}")
+                file.write(f"{key}: {value} kr\n")
+            file.write(f"Summan av dina kostnader är: {calculated_costs()}\n") #write the calculated costs to the budget file
+            file.write(f"Du har såhär mycket pengar över: {calculated_excess(salary)}")
             file.close()
             break
         elif question == "nej":
